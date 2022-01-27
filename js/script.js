59b6e9a13ff6f981313be3d4bdaf2adfecdee8a4
Jeremy Yochum <jyochum391@yahoo.com>
2022-01-27 16:30:14 -0500

    Update script.js

--- js/script.js
+++ js/script.js
@@ -29,5 +29,5 @@ Project 4 - Random Quote Generator
  * click event listener for the print quote button
  * DO NOT CHANGE THE CODE BELOW!!
 ***/
-
-document.getElementById('load-quote').addEventListener("click", printQuote, false);
+console.log('TEST');
+// document.getElementById('load-quote').addEventListener("click", printQuote, false);

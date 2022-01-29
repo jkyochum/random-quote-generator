893c8756cb69a747014a9214afa66b4323594587
Jeremy Yochum <jyochum391@yahoo.com>
2022-01-29 14:41:20 -0500

    updated quotes array

--- js/script.js
+++ js/script.js
@@ -10,7 +10,13 @@ Project 4 - Random Quote Generator
 /*** 
  * `quotes` array 
 ***/
-
+const quotes = [
+  {quote: 'Live in the present, remember the past, and fear not the future, for it doesn\'t exist and never shall. There is only now.', source: 'Christopher Paolini', citation: 'Eldest', year: '2005'},
+  {quote: 'Instead of worrying about what you cannot control, shift your energy to what you can create.', source: 'Roy T. Bennett', citation: 'The Light in the Heart', year: '2016'},
+  {quote: 'War is peace. Freedom is slavery. Ignorance is strength.', source: 'George Orwell', citation: '1984', year: '1949'},
+  {quote: 'All the darkness in the world cannot extinguish the light of a single candle.', source: 'Ugolino Brunforte', citation: 'Little Flowers of St. Francis', year: 'near the end of the 14th century'},
+  {quote: `...you're off to Great Places! Today is your day! Your mountain is waiting. So... <i>get on your way!</i>`, source: 'Dr. Seuss', citation: 'Oh, the Places You\ll Go!', year: '1960'}
+];
 
 
 /***
@@ -29,5 +35,4 @@ Project 4 - Random Quote Generator
  * click event listener for the print quote button
  * DO NOT CHANGE THE CODE BELOW!!
 ***/
-console.log('TEST');
-// document.getElementById('load-quote').addEventListener("click", printQuote, false);
+document.getElementById('load-quote').addEventListener("click", printQuote, false);

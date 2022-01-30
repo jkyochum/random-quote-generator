33f5393414b671090713461ae357513a333017a5
Jeremy Yochum <jyochum391@yahoo.com>
2022-01-30 17:47:47 -0500

    updated script.js, index.html, and styles.css

--- js/script.js
+++ js/script.js
@@ -8,19 +8,19 @@ Project 4 - Random Quote Generator
   // Reach out in your Slack community
 
 /*** 
- * `quotes` array 
+ * create array of quotes 
 ***/
 const quotes = [
   {quote: 'Live in the present, remember the past, and fear not the future, for it doesn\'t exist and never shall. There is only now.', source: 'Christopher Paolini', citation: 'Eldest', year: '2005'},
   {quote: 'Instead of worrying about what you cannot control, shift your energy to what you can create.', source: 'Roy T. Bennett', citation: 'The Light in the Heart', year: '2016'},
   {quote: 'War is peace. Freedom is slavery. Ignorance is strength.', source: 'George Orwell', citation: '1984', year: '1949'},
   {quote: 'All the darkness in the world cannot extinguish the light of a single candle.', source: 'Ugolino Brunforte', citation: 'Little Flowers of St. Francis', year: 'near the end of the 14th century'},
-  {quote: `...you're off to Great Places! Today is your day! Your mountain is waiting. So... <i>get on your way!</i>`, source: 'Dr. Seuss', citation: 'Oh, the Places You\ll Go!', year: '1960'}
+  {quote: `...you're off to Great Places! Today is your day! Your mountain is waiting. So... <i>get on your way!</i>`, source: 'Dr. Seuss', citation: 'Oh, the Places You\'ll Go!', year: '1960'}
 ];
 
 
 /***
- * `getRandomQuote` function
+ * create random number and return quote based on array index
 ***/
 function getRandomQuote(arr){
   const randomNumber = Math.floor(Math.random() * arr.length);
@@ -29,9 +29,8 @@ function getRandomQuote(arr){
 console.log(getRandomQuote(quotes));
 
 
-
 /***
- * `printQuote` function
+ * create html string from quote object created in getRandomQuote
 ***/
 function printQuote(){
   const obj = getRandomQuote(quotes);
@@ -45,17 +44,25 @@ function printQuote(){
     <p class="source">${source}
   `;
 
-  if(quotes.citation){
-    source.insertAdjacentHTML(beforeend, `<span class="citation">${citation}</span>`);
+  //setting the available content into span tags 
+  if(citation){
+    html += `<span class="citation">, ${citation}</span>`;
   }
-  if(quotes.year){
-    html += `<span class="year">${year}</span>`;
+  else{
+    html += `<span class="citation"></span>`;
+  }
+  if(year){
+    html += `<span class="year">, ${year}</span>`;
+  }
+  else{
+    html += `<span class="year"></span>`;
   }
 
+  //adding the ending paragraph tag
   html += `</p>`;
 
+  //setting the string into the existing html
   document.getElementById('quote-box').innerHTML = html;
-
 }
 
 
